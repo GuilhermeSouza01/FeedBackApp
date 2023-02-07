a056eb2de065f5668b0b48d505d3a4e91d840ad8
Guilherme Souza <ghostgui60@gmail.com>
2023-02-07 20:01:01 -0300

    Create a readme to project

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,6 +1,5 @@
-import { async } from "q";
 import { createContext, useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {

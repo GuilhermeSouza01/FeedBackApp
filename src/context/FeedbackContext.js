4955edcf9f98134095bb2da9263e4b9a36124cba
Guilherme Souza <ghostgui60@gmail.com>
2023-02-07 13:44:27 -0300

    Initialize the json-server for backend mockup

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -3,23 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This feedback item 1",
-      rating: 9,
-    },
-    {
-      id: 2,
-      text: "This feedback item 2",
-      rating: 10,
-    },
-    {
-      id: 3,
-      text: "This feedback item 3",
-      rating: 8,
-    },
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
